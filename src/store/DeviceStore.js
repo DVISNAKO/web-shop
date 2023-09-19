0f7666e425602f1ec8e838e8a55499a6f4b81b00
Dima <dmitrijs.visnakovs@inbox.lv>
2023-09-19 15:44:32 +0300

    add typeBar

--- src/store/DeviceStore.js
+++ src/store/DeviceStore.js
@@ -4,33 +4,54 @@ export default class deviceStore {
     constructor(){
         this._types = [
             {id: 1, name: 'Холодильник'},
-            {id: 2, name: 'Смартфон'}
+            {id: 2, name: 'Смартфон'},
+            {id: 3, name: 'Ноутбуки'},
+            {id: 4, name: 'Телевизоры'}
         ]
         this._brands = [
             {id: 1, name: 'Samsung'},
             {id: 2, name: 'Apple'},
         ]
-        this.devices = [
+        this._devices = [
             {id: 1, name: 'Iphone 12pro', price: 25000, rating: 4},
             {id: 1, name: 'Iphone 13pro', price: 35000, rating: 5},
         ]
 
+        this._selectedType = {};
+
         makeAutoObservable(this);
     }
 
-    setIsAuth(bool){
-        this._isAuth = bool;
+    setTypes(types){
+        this._types = types;
     }
 
-    setIsUser(user){
-        this._user = user;
+    setBrands(brands){
+        this._brands = brands;
     }
 
-    get isAuth(){
-        return this._isAuth
+    setDevices(devices){
+        this._devices = devices;
     }
 
-    get getUser(){
-        return this._user
+    setSelectedType(type){
+        this._selectedType = type;
     }
+
+    get types(){
+        return this._types;
+    }
+
+    get brands(){
+        return this._brands;
+    }
+
+    get devices(){
+        return this._devices;
+    }
+
+    get selectedType(){
+        return this._selectedType;
+    }
+
 }
