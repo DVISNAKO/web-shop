a926c809bbd4e67f42730174f7e051a28a202772
Dima <dmitrijs.visnakovs@inbox.lv>
2023-09-20 13:56:48 +0300

    add navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,15 @@ import { Context } from "../index";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink } from "react-router-dom";
-import { SHOP_ROUTE } from "../util/consts";
+import { NavLink, useNavigate } from "react-router-dom";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../util/consts";
 import Button from "react-bootstrap/Button";
 import {observer} from "mobx-react-lite";
 
+
 const NavBar = observer(() => {
   const { user } = useContext(Context);
+  const navigate = useNavigate();
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
@@ -17,8 +19,15 @@ const NavBar = observer(() => {
           <NavLink to={SHOP_ROUTE}>КупиДевайс</NavLink>
           {user.isAuth ? (
             <Nav className="ml-auto">
-              <Button variant={"outline-light"}>Админ панель</Button>
-              <Button variant={"outline-light"} style={{marginLeft: 7}}>Войти</Button>
+              <Button 
+              variant={"outline-light"} 
+              onClick={() => navigate(ADMIN_ROUTE)} 
+              >Админ панель</Button>
+              <Button 
+              variant={"outline-light"} 
+              onClick={() => navigate(LOGIN_ROUTE)} 
+              style={{marginLeft: 7}}
+              >Выйти</Button>
             </Nav>
           ) : (
             <Nav className="ml-auto">
